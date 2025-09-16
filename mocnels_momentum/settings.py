b049adf96da6325c04ed051d67d8aa930cbf6fcb
DerNiks <derrick@ui.ac.id>
2025-09-16 23:07:04 +0700

    fix : Memperbaiki csrf

--- mocnels_momentum/settings.py
+++ mocnels_momentum/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "derrick-mocnelsmomentum.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "derrick-mocnelsmomentum.pbp.cs.ui.ac.id"
+    "https://derrick-mocnelsmomentum.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
